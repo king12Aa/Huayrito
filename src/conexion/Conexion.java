016a134207084f2778f581995e3e63fea25835d7
Alejandro15927 <alejandroparedeseguia@gmail.com>
2024-12-09 01:43:57 -0500

    Final

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package conexion;
+package Conexion;
 
-/**
- *
- * @author USER
- */
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
 public class Conexion {
     
+    Connection cn;
+    
+    public Connection ConectarBD() {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            cn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost/huayrito","root","");
+            System.out.println("Conexion exitosa");
+        }catch (Exception e){
+            System.out.println("Conexion fallida " + e);
+        }
+        return cn;
+    }
+
 }
